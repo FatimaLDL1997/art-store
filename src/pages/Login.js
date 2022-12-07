9a632d29e94cdef4e5a590f5ead47f206de404d2
Fatima Labade <fatima.labade@ontariotechu.net>
2022-12-06 22:53:05 -0500

    fixed front

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -8,13 +8,13 @@ const Login = () => {
   return (
     <Wrapper>
       <div className='container'>
-        <img src={loginImg} alt='user' />
-      </div>
-      <div className='card'>
-        <h1>Welcome!</h1>
-        <button className='btn' onClick={loginWithRedirect}>
-          Login/Sign Up
-        </button>
+        <img src={loginImg} className='background-img' alt='user' />
+        <div className='card'>
+          <h1>Welcome!</h1>
+          <button className='btn' onClick={loginWithRedirect}>
+            Login/Sign Up
+          </button>
+        </div>
       </div>
     </Wrapper>
   );
@@ -23,6 +23,7 @@ const Wrapper = styled.section`
   min-height: 100vh;
   display: grid;
   place-items: center;
+
   .container {
     position: absolute;
     width: 100vw;
@@ -31,20 +32,23 @@ const Wrapper = styled.section`
     display: flex;
     justify-content: center;
     align-items: center;
+    overflow: hidden !important;
+  }
+  .background-img {
+    position: relative;
+    width: auto;
+    height: 100vh;
   }
   .card {
     position: absolute;
-    width: 31rem;
-    height: 18rem;
+    width: 30vw;
+    height: 50vh;
     background: #dd6969;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-  }
-  img {
-    width: 167vw;
-    height: 100vh;
+    overflow: none;
   }
   h1 {
     top: 50vh;
